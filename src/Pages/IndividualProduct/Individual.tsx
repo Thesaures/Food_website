05252edbad2ba46f9fc298821a2c3bcf307ec85d
Ashik George <ashikgeorgec@gmail.com>
2024-07-17 12:24:12 +0530

    Revert "added individual page"
    This reverts commit 22175e0c22686b0712de9c768963147fb1537b28.

--- src/Pages/IndividualProduct/Individual.tsx
+++
@@ -1,8 +0,0 @@
-import "./Individual.css";
-import { useParams } from "react-router-dom";
-const Individual = () => {
-    const params = useParams();
-    const { num } = params;
-    return <p>this is individual product page {num}</p>;
-};
-export default Individual;
